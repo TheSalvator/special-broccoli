ce8a1b325f2304ab8861a1eb95ce67540e3d63dc
Ilya Kazakov <kazakovilya97@gmail.com>
2023-01-07 20:34:58 +0300

    Update init.lua
    Render whitespaces correctly
    Split right and below

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,6 +6,9 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
-vim.opt.listchars = { space = '·', tab = '›—' }
+vim.opt.listchars = { space = '·', tab = '——›' }
 vim.opt.list = true
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
 
